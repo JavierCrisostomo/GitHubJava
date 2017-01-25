0bfc6fc756cd1126a3b48d562453c04b4382e87b
Javier Crisostomo <crisostomo.almeyda@gmail.com>
2017-01-25 18:20:19 -0500

    New commit.

--- LearningJava/src/com/java/orion/TestUI.java
+++ LearningJava/src/com/java/orion/TestUI.java
@@ -1,16 +1,14 @@
 package com.java.orion;
+import java.awt.Image;
 import java.net.MalformedURLException;
 import java.net.URL;
-
 import javax.swing.*;
+
 public class TestUI {
 
 	public static void main(String[] args) throws MalformedURLException
 	  {
-	    String backupDir = "/Users/al/backups";
-	    final ImageIcon icon = new ImageIcon(new URL("http://www.gravatar.com/avatar/a1ab0af4997654345d7a949877f8037e?s=128&d=identicon&r=PG"));
-	    
-	    // create a jframe
+	    //Creating a JFrame
 	    JFrame frame = new JFrame("JOptionPane showMessageDialog example");
 	    frame.setVisible(true);
 	    frame.setSize(400, 250);
@@ -23,32 +21,50 @@ public class TestUI {
 	    btnBuscar.setText("Buscar");
 	    //frame.add(btnBuscar);   
 	    
-	    // show a joptionpane dialog using showMessageDialog
+	    //Show a showMessagedialog with different icon
 	    JOptionPane.showMessageDialog(null,
 	    		"Promises and more Promises'.",
 	    		"Estudiando Java2",
-	    		JOptionPane.QUESTION_MESSAGE, icon);
+	    		JOptionPane.QUESTION_MESSAGE);
 	    
 	    JOptionPane.showMessageDialog(null,
 	    		"Promises and more Promises'.",
 	    		"Estudiando Java2",
-	    		JOptionPane.INFORMATION_MESSAGE, icon);
+	    		JOptionPane.INFORMATION_MESSAGE);
 	    
 	    JOptionPane.showMessageDialog(null,
 	    		"Promises and more Promises'.",
 	    		"Estudiando Java2",
-	    		JOptionPane.ERROR_MESSAGE, icon);
+	    		JOptionPane.ERROR_MESSAGE);
 	    
 	    JOptionPane.showMessageDialog(null,
 		        "Promises and more Promises'.",
 		        "Estudiando Java2",
-		        JOptionPane.WARNING_MESSAGE, icon);
+		        JOptionPane.WARNING_MESSAGE);
 	    
+	    JOptionPane.showMessageDialog(null,
+		        "Promises and more Promises'.",
+		        "Estudiando Java2",
+		        JOptionPane.PLAIN_MESSAGE);
+
+	    final ImageIcon icon = new ImageIcon(new URL("http://www.gravatar.com/avatar/a1ab0af4997654345d7a949877f8037e?s=128&d=identicon&r=PG"));
+	    Image img = icon.getImage();
+	    Image newImg = img.getScaledInstance(35, 40,  java.awt.Image.SCALE_SMOOTH);
+	    ImageIcon newIcon = new ImageIcon(newImg);
+
+	    //Using an icon  
 	    JOptionPane.showMessageDialog(null,
 		        "Promises and more Promises'.",
 		        "Estudiando Java2",
 		        JOptionPane.PLAIN_MESSAGE, icon);
-	   
+	    
+	    //Now using a resized icon
+	    JOptionPane.showMessageDialog(null,
+		        "Promises and more Promises'.",
+		        "Estudiando Java2",
+		        JOptionPane.PLAIN_MESSAGE, newIcon);
+
+	    
 	    System.exit(0);
 	  }
 }
