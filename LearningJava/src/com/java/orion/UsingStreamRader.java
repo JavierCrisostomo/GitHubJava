eaed5c2165b9f4caabbc52db6246e61658893976
Javier Crisostomo <crisostomo.almeyda@gmail.com>
2017-01-26 11:32:36 -0500

    File name changed.

--- LearningJava/src/com/java/orion/UsingStreamRader.java
+++ LearningJava/src/com/java/orion/UsingStreamRader.java
@@ -3,7 +3,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class TestStreamRader {
+public class UsingStreamRader {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br =  new BufferedReader(new InputStreamReader(System.in));
 		System.out.println("Por favor ingrese su nombre:");
